8b8674600cf57dc9e69584ad11035d6f44eaeeb7
haireal007 <you@example.com>
2022-09-12 08:32:04 +0700

    hai1

---
+++ src/app/script.store.ts
@@ -0,0 +1,8 @@
+export interface Scripts {
+  name: string;
+  src: string;
+}
+export const ScriptStore: Scripts[] = [
+  {name: 'asst', src: "/assets/css/core-style.css"},
+  {name: 'rangeSlider', src: '../../../assets/js/ion.rangeSlider.min.js'}
+];
