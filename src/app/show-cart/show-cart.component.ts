5bb2e7a61e7128ed138d7e7ac1aed44d617ff632
haireal007 <you@example.com>
2022-09-13 13:37:36 +0700

    2h13/9

--- src/app/show-cart/show-cart.component.ts
+++ src/app/show-cart/show-cart.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OderService} from "../service/OderService";
 import {OderProduct} from "../model/OderProduct";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Product} from "../model/Product";
 
 @Component({
@@ -10,14 +10,32 @@ import {Product} from "../model/Product";
   styleUrls: ['./show-cart.component.css']
 })
 export class ShowCartComponent implements OnInit {
-  constructor(private route: ActivatedRoute,private oderService: OderService) {
+
+  constructor(private route: ActivatedRoute, private oderService: OderService,private router: Router) {
   }
 
   oderProducts: OderProduct[] = [];
   products: Product[] = [];
+
   ngOnInit(): void {
     // @ts-ignore
     this.products = JSON.parse(localStorage.getItem("cart"));
     console.log("hoang" + this.products)
   }
+
+  delete(id: any): void {
+    // @ts-ignore
+    this.products = JSON.parse(localStorage.getItem("cart"));
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.products[i].id == id) {
+        this.products.splice(i)
+        break;
+      }
+    }
+    console.log(this.products)
+    localStorage.setItem("cart", JSON.stringify(this.products));
+    this.router.navigate(["/showcart"])
+  }
+
+
 }
--- src/app/show-product-dt/show-product-dt.component.ts
+++ src/app/show-product-dt/show-product-dt.component.ts
@@ -30,27 +30,31 @@ export class ShowProductDTComponent implements OnInit {
 
   create(): void {
     this.route.params.subscribe(paramsId => {
-
       let id = paramsId?.['id'];
+      console.log(id)
       this.productService.findById(id).subscribe((data: Product) => {
         let a = true;
+        console.log(this.products)
         if (this.products == null) {
           this.products = [];
           this.products.push(data);
-        } else {
-          for (let i = 0; i < this.products.length; i++) {
-            if (id == this.products[i].id) {
-              a = true
-              break;
-            } else {
-              a = false;
-            }
-          }
-
-          if (a == false) {
-            this.products.push(data);
-          }
         }
+
+       if (  this.products != []){
+         for (let i = 0; i < this.products.length; i++) {
+           if (id == this.products[i].id) {
+             this.products[i].amount= this.products[i].amount+1;
+             localStorage.setItem("cart", JSON.stringify(this.products));
+             this.router.navigate(["/showcart"])
+             return
+           }
+         }
+
+       }
+
+        this.products.push(data);
+
+
         localStorage.setItem("cart", JSON.stringify(this.products));
         this.router.navigate(["/showcart"])
       })
