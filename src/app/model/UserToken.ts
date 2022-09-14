3f8ac9be1c9663b89efbe8c1d2e2f71d059b5718
hoanghai1310 <hoanghai.131096@gmail.com>
2022-09-14 15:41:35 +0700

    doneloginregister14092022

--- src/app/model/UserToken.ts
+++ src/app/model/UserToken.ts
@@ -1,13 +1,14 @@
+import {Role} from "./role";
 
 
 export class UserToken{
   id: number;
   name: string;
   token:string;
-  roles: any;
+  roles: Role[];
 
 
-  constructor(id: number, name: string, token: string, roles: any) {
+  constructor(id: number, name: string, token: string, roles: Role[]) {
     this.id = id;
     this.name = name;
     this.token = token;
