f2ea64a985dd567c56f41b7d8197a336480c66a0
haireal007 <you@example.com>
2022-09-15 17:14:32 +0700

    hai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,6 @@ const routes: Routes = [
   {path: 'detail/:id', component: ShowProductDTComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent}
-
 ];
 
 @NgModule({
